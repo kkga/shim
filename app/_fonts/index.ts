23362da7727a7df7b085caeebee3a27444d32567
Gadzhi Kharkharov <me@kkga.me>
2024-04-30 23:41:54 +0300

    upd

--- app/_fonts/index.ts
+++ app/_fonts/index.ts
@@ -1,18 +1,18 @@
 import localFont from 'next/font/local'
 
 export const fontMono = localFont({
-  src: './JetbrainsMono-Regular.woff2',
+  src: '@/_fonts/JetbrainsMono-Regular.woff2',
   variable: '--font-mono',
 })
 
 export const fontSans = localFont({
   src: [
     {
-      path: './InterVariable.woff2',
+      path: '@/_fonts/InterVariable.woff2',
       style: 'normal',
     },
     {
-      path: './InterVariable-Italic.woff2',
+      path: '@/_fonts/InterVariable-Italic.woff2',
       style: 'italic',
     },
   ],
