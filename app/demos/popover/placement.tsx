6911328c191e4e9f910125322de4ba3a3c13f301
Gadzhi Kharkharov <me@kkga.me>
2024-05-06 21:44:14 +0300

    docs: add padding to popover demo

--- app/demos/popover/placement.tsx
+++ app/demos/popover/placement.tsx
@@ -1,6 +1,6 @@
-import { ArrowDownRight, ArrowLeft, ArrowRight, ArrowUpLeft } from '@phosphor-icons/react/dist/ssr'
-import { Button } from '@ui/button'
-import { Popover, PopoverTrigger } from '@ui/popover'
+import { ArrowDownRight, ArrowLeft, ArrowRight, ArrowUpLeft } from "@phosphor-icons/react/dist/ssr"
+import { Button } from "@ui/button"
+import { Popover, PopoverTrigger } from "@ui/popover"
 
 export default () => (
   <>
@@ -8,28 +8,36 @@ export default () => (
       <Button>
         <ArrowUpLeft size={16} />
       </Button>
-      <Popover placement='left bottom'>Left bottom</Popover>
+      <Popover className="p-4" placement="left bottom">
+        Left bottom
+      </Popover>
     </PopoverTrigger>
 
     <PopoverTrigger>
       <Button>
         <ArrowLeft size={16} />
       </Button>
-      <Popover placement='left'>Left</Popover>
+      <Popover className="p-4" placement="left">
+        Left
+      </Popover>
     </PopoverTrigger>
 
     <PopoverTrigger>
       <Button>
         <ArrowRight size={16} />
       </Button>
-      <Popover placement='right'>Right</Popover>
+      <Popover className="p-4" placement="right">
+        Right
+      </Popover>
     </PopoverTrigger>
 
     <PopoverTrigger>
       <Button>
         <ArrowDownRight size={16} />
       </Button>
-      <Popover placement='right top'>Right top</Popover>
+      <Popover className="p-4" placement="right top">
+        Right top
+      </Popover>
     </PopoverTrigger>
   </>
 )
