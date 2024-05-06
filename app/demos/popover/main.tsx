f36afefc24c687ce3ad639b3a19d598b293451ee
Gadzhi Kharkharov <me@kkga.me>
2024-05-06 16:54:42 +0300

    docs: add padding to popover demo

--- app/demos/popover/main.tsx
+++ app/demos/popover/main.tsx
@@ -1,14 +1,17 @@
-import { Button } from '@ui/button'
-import { Popover, PopoverTrigger } from '@ui/popover'
-import { Slider } from '@ui/slider'
+import { SlidersHorizontal } from "@phosphor-icons/react/dist/ssr"
+import { Button } from "@ui/button"
+import { Popover, PopoverTrigger } from "@ui/popover"
+import { Slider } from "@ui/slider"
 
 export default () => (
   <PopoverTrigger>
-    <Button>Dimensions</Button>
-    <Popover className='w-[200px]' placement='bottom start'>
-      <div className='grid gap-4'>
-        <Slider label='Width' defaultValue={73} isFilled />
-        <Slider label='Height' defaultValue={37} isFilled />
+    <Button>
+      <SlidersHorizontal size={16} weight="duotone" />
+    </Button>
+    <Popover className="w-[200px] p-4" placement="bottom start">
+      <div className="grid gap-4">
+        <Slider label="Width" defaultValue={37} isFilled />
+        <Slider label="Height" defaultValue={73} isFilled />
       </div>
     </Popover>
   </PopoverTrigger>
