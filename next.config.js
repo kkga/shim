f3bc801d9f84ec4de00eeb4978a78826368761df
Gadzhi Kharkharov <me@kkga.me>
2024-05-08 11:55:49 +0300

    chore: minor

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const localesPlugin = require("@react-aria/optimize-locales-plugin")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['vscode-oniguruma', 'shiki'],
+  experimental: {},
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(localesPlugin.webpack({ locales: [] }))
+    }
+    return config
   },
 }
 
