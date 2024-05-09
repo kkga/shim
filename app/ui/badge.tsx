b5f5d3bdef7d1ac685a2d3af8af5d24c9a99f232
Gadzhi Kharkharov <me@kkga.me>
2024-05-10 00:46:48 +0300

    fix: tweak badge padding in size 1

--- app/ui/badge.tsx
+++ app/ui/badge.tsx
@@ -19,7 +19,7 @@ const badgeStyles = cva({
       error: "border border-error-line bg-error-bg-subtle text-error-text",
     },
     size: {
-      1: "h-4 px-0.5 gap-1 rounded text-[11px]",
+      1: "h-4 px-1 gap-1 rounded text-[11px]",
       2: "h-5 px-1 gap-1 rounded",
       3: "h-6 px-1.5 gap-1.5 rounded-md",
     },
