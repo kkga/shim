00cb9d37b1339aa7b43684462567aefd15a8cfe1
Gadzhi Kharkharov <me@kkga.me>
2024-05-10 16:55:15 +0300

    fix: show focus outline for group inputs

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cva'
-import { composeRenderProps } from 'react-aria-components'
-import { twMerge } from 'tailwind-merge'
+import { defineConfig } from "cva"
+import { composeRenderProps } from "react-aria-components"
+import { twMerge } from "tailwind-merge"
 
 export const { compose, cx, cva } = defineConfig({
   hooks: { onComplete: (className) => twMerge(className) },
@@ -13,35 +13,35 @@ export const cxRenderProps = <T>(
   composeRenderProps(className, (className) => cx(tw, className))
 
 export const Intents = [
-  'neutral',
-  'accent',
-  'success',
-  'warning',
-  'error',
+  "neutral",
+  "accent",
+  "success",
+  "warning",
+  "error",
 ] as const
 
 export type Intent = (typeof Intents)[number]
 
 export const animateMountStyle = cva({
   base: [
-    'data-[exiting]:animate-[fade-out_100ms]',
-    'data-[placement=bottom]:animate-[fade-in_100ms,slide-from-bottom_100ms]',
-    'data-[placement=top]:animate-[fade-in_100ms,slide-from-top_100ms]',
-    'data-[placement=right]:animate-[fade-in_100ms,slide-from-right_100ms]',
-    'data-[placement=left]:animate-[fade-in_100ms,slide-from-left_100ms]',
+    "data-[exiting]:animate-[fade-out_100ms]",
+    "data-[placement=bottom]:animate-[fade-in_100ms,slide-from-bottom_100ms]",
+    "data-[placement=top]:animate-[fade-in_100ms,slide-from-top_100ms]",
+    "data-[placement=right]:animate-[fade-in_100ms,slide-from-right_100ms]",
+    "data-[placement=left]:animate-[fade-in_100ms,slide-from-left_100ms]",
   ],
 })
 
 export const focusStyle = cva({
   base: [
-    'outline-offset-2 outline-accent-focus-ring outline-0',
-    'data-[focus-visible]:outline-2',
+    "outline-offset-2 outline-accent-focus-ring outline-0",
+    "data-[focus-visible]:outline-2 group-data-[focus-visible]:outline-2",
   ],
 })
 
 export const focusInsetStyle = cva({
   base: [
-    '-outline-offset-1 outline-accent-focus-ring outline-0',
-    'data-[focus-visible]:outline-1',
+    "-outline-offset-1 outline-accent-focus-ring outline-0",
+    "data-[focus-visible]:outline-1 group-data-[focus-visible]:outline-1",
   ],
 })
