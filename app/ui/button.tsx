f79957b8e7932b14c4b277b313626f6adc466882
Gadzhi Kharkharov <me@kkga.me>
2024-05-14 17:02:34 +0300

    refactor: add LinkButton component to Button

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -7,174 +7,161 @@ import {
   cxRenderProps,
   focusStyle,
   type Intent,
-} from "@lib/utils"
+} from "@lib/styleUtils"
 
 import type { VariantProps } from "cva"
 import { Children, isValidElement } from "react"
 import {
+  Link,
+  LinkProps,
   Button as RACButton,
   type ButtonProps as RACButtonProps,
 } from "react-aria-components"
 
-const buttonStyle = cva({
-  base: [
-    "font-medium inline-flex items-center justify-center shrink-0 truncate",
-    // disabled
-    "data-disabled:text-neutral-placeholder data-disabled:bg-neutral-bg-subtle",
-  ],
-  variants: {
-    variant: {
-      soft: null,
-      solid: "text-white",
-      ghost: "bg-transparent",
-    },
-    intent: Intents.reduce(
-      (acc, intent) => {
-        acc[intent] = ""
-        return acc
+const style = compose(
+  focusStyle,
+  cva({
+    base: [
+      "font-medium inline-flex items-center justify-center shrink-0 truncate",
+      // disabled
+      "data-disabled:text-neutral-placeholder data-disabled:bg-neutral-bg-subtle data-disabled:inset-ring data-disabled:inset-ring-neutral-line",
+    ],
+    variants: {
+      variant: {
+        soft: null,
+        solid: "text-white",
+        ghost: "bg-transparent",
       },
-      {} as Record<Intent, string>,
-    ),
-    size: {
-      1: "text-xs h-6 px-1.5 rounded gap-1.5",
-      2: "text-[13px] h-7 px-2 rounded gap-2",
-      3: "text-sm h-8 px-2.5 rounded-md gap-2",
+      intent: Intents.reduce(
+        (acc, intent) => {
+          acc[intent] = ""
+          return acc
+        },
+        {} as Record<Intent, string>,
+      ),
+      size: {
+        1: "text-xs h-6 px-1.5 rounded gap-1.5",
+        2: "text-[13px] h-7 px-2 rounded gap-2",
+        3: "text-sm h-8 px-2.5 rounded-md gap-2",
+      },
+      isSquare: { true: null, false: null },
     },
-    isSquare: { true: null, false: null },
-  },
-  compoundVariants: [
-    { size: 1, isSquare: true, className: "size-6 p-0" },
-    { size: 2, isSquare: true, className: "size-7 p-0" },
-    { size: 3, isSquare: true, className: "size-8 p-0" },
-    {
-      intent: "neutral",
-      variant: "soft",
-      className:
-        "text-neutral-text bg-neutral-bg hover:bg-neutral-bg-hover active:bg-neutral-bg-active",
-    },
-    {
-      intent: "neutral",
-      variant: "ghost",
-      className:
-        "text-neutral-text bg-transparent hover:bg-neutral-bg-hover active:bg-neutral-bg-active",
-    },
-    {
-      intent: "neutral",
-      variant: "solid",
-      className:
-        "text-white bg-neutral-solid hover:bg-neutral-solid-hover active:brightness-90",
-    },
+    compoundVariants: [
+      { size: 1, isSquare: true, className: "size-6 p-0" },
+      { size: 2, isSquare: true, className: "size-7 p-0" },
+      { size: 3, isSquare: true, className: "size-8 p-0" },
+      {
+        intent: "neutral",
+        variant: "soft",
+        className:
+          "text-neutral-text bg-neutral-bg data-hovered:bg-neutral-bg-hover data-pressed:bg-neutral-bg-active",
+      },
+      {
+        intent: "neutral",
+        variant: "ghost",
+        className:
+          "text-neutral-text bg-transparent data-hovered:bg-neutral-bg-hover data-pressed:bg-neutral-bg-active",
+      },
+      {
+        intent: "neutral",
+        variant: "solid",
+        className:
+          "text-white bg-neutral-solid data-hovered:bg-neutral-solid-hover data-pressed:brightness-90",
+      },
 
-    {
-      intent: "accent",
-      variant: "soft",
-      className:
-        "text-accent-text bg-accent-bg hover:bg-accent-bg-hover active:bg-accent-bg-active",
-    },
-    {
-      intent: "accent",
-      variant: "ghost",
-      className:
-        "text-accent-text bg-transparent hover:bg-accent-bg-hover active:bg-accent-bg-active",
-    },
-    {
-      intent: "accent",
-      variant: "solid",
-      className:
-        "text-white bg-accent-solid hover:bg-accent-solid-hover active:brightness-90",
-    },
+      {
+        intent: "accent",
+        variant: "soft",
+        className:
+          "text-accent-text bg-accent-bg data-hovered:bg-accent-bg-hover data-pressed:bg-accent-bg-active",
+      },
+      {
+        intent: "accent",
+        variant: "ghost",
+        className:
+          "text-accent-text bg-transparent data-hovered:bg-accent-bg-hover data-pressed:bg-accent-bg-active",
+      },
+      {
+        intent: "accent",
+        variant: "solid",
+        className:
+          "text-white bg-accent-solid data-hovered:bg-accent-solid-hover data-pressed:brightness-90",
+      },
 
-    {
-      intent: "success",
-      variant: "soft",
-      className:
-        "text-success-text bg-success-bg hover:bg-success-bg-hover active:bg-success-bg-active",
-    },
-    {
-      intent: "success",
-      variant: "ghost",
-      className:
-        "text-success-text bg-transparent hover:bg-success-bg-hover active:bg-success-bg-active",
-    },
-    {
-      intent: "success",
-      variant: "solid",
-      className:
-        "text-white bg-success-solid hover:bg-success-solid-hover active:brightness-90",
-    },
+      {
+        intent: "success",
+        variant: "soft",
+        className:
+          "text-success-text bg-success-bg data-hovered:bg-success-bg-hover data-pressed:bg-success-bg-active",
+      },
+      {
+        intent: "success",
+        variant: "ghost",
+        className:
+          "text-success-text bg-transparent data-hovered:bg-success-bg-hover data-pressed:bg-success-bg-active",
+      },
+      {
+        intent: "success",
+        variant: "solid",
+        className:
+          "text-white bg-success-solid data-hovered:bg-success-solid-hover data-pressed:brightness-90",
+      },
 
-    {
-      intent: "warning",
-      variant: "soft",
-      className:
-        "text-warning-text bg-warning-bg hover:bg-warning-bg-hover active:bg-warning-bg-active",
-    },
-    {
-      intent: "warning",
-      variant: "ghost",
-      className:
-        "text-warning-text bg-transparent hover:bg-warning-bg-hover active:bg-warning-bg-active",
-    },
-    {
-      intent: "warning",
-      variant: "solid",
-      className:
-        "text-white bg-warning-solid hover:bg-warning-solid-hover active:brightness-90",
-    },
+      {
+        intent: "warning",
+        variant: "soft",
+        className:
+          "text-warning-text bg-warning-bg data-hovered:bg-warning-bg-hover data-pressed:bg-warning-bg-active",
+      },
+      {
+        intent: "warning",
+        variant: "ghost",
+        className:
+          "text-warning-text bg-transparent data-hovered:bg-warning-bg-hover data-pressed:bg-warning-bg-active",
+      },
+      {
+        intent: "warning",
+        variant: "solid",
+        className:
+          "text-white bg-warning-solid data-hovered:bg-warning-solid-hover data-pressed:brightness-90",
+      },
 
-    {
-      intent: "error",
-      variant: "soft",
-      className:
-        "text-error-text bg-error-bg hover:bg-error-bg-hover active:bg-error-bg-active",
-    },
-    {
-      intent: "error",
-      variant: "ghost",
-      className:
-        "text-error-text bg-transparent hover:bg-error-bg-hover active:bg-error-bg-active",
-    },
-    {
-      intent: "error",
-      variant: "solid",
-      className:
-        "text-white bg-error-solid hover:bg-error-solid-hover active:brightness-90",
-    },
-  ],
-  defaultVariants: { intent: "accent", variant: "soft", size: 1 },
-})
+      {
+        intent: "error",
+        variant: "soft",
+        className:
+          "text-error-text bg-error-bg data-hovered:bg-error-bg-hover data-pressed:bg-error-bg-active",
+      },
+      {
+        intent: "error",
+        variant: "ghost",
+        className:
+          "text-error-text bg-transparent data-hovered:bg-error-bg-hover data-pressed:bg-error-bg-active",
+      },
+      {
+        intent: "error",
+        variant: "solid",
+        className:
+          "text-white bg-error-solid data-hovered:bg-error-solid-hover data-pressed:brightness-90",
+      },
+    ],
+    defaultVariants: { intent: "accent", variant: "soft", size: 1 },
+  }),
+)
 
-const styles = compose(focusStyle, buttonStyle)
+interface ButtonProps extends RACButtonProps, VariantProps<typeof style> {}
 
-interface ButtonProps extends RACButtonProps, VariantProps<typeof styles> {}
-
-function Button({
-  className,
-  variant,
-  intent,
-  size,
-  isSquare,
-  ...props
-}: ButtonProps) {
-  let children =
-    typeof props.children !== "function" && Children.toArray(props.children)
-
-  let hasOnlySvg =
-    children &&
-    children.length === 1 &&
-    isValidElement(children[0]) &&
-    children[0].type === "svg"
-
+function Button({ className, ...props }: ButtonProps) {
   return (
     <RACButton
       className={cxRenderProps(
         className,
-        styles({
-          variant,
-          intent,
-          size,
-          isSquare: typeof isSquare === "boolean" ? isSquare : hasOnlySvg,
-          className,
+        style({
+          ...props,
+          isSquare:
+            typeof props.isSquare === "boolean" ?
+              props.isSquare
+            : hasOnlySvgChild(props),
         }),
       )}
       {...props}
@@ -182,4 +169,37 @@ function Button({
   )
 }
 
-export { Button, buttonStyle, type ButtonProps }
+function LinkButton({
+  className,
+  ...props
+}: LinkProps & VariantProps<typeof style>) {
+  return (
+    <Link
+      className={cxRenderProps(
+        className,
+        style({
+          ...props,
+          isSquare:
+            typeof props.isSquare === "boolean" ?
+              props.isSquare
+            : hasOnlySvgChild(props),
+        }),
+      )}
+      {...props}
+    />
+  )
+}
+
+function hasOnlySvgChild(props: any): boolean {
+  let children =
+    typeof props.children !== "function" && Children.toArray(props.children)
+
+  return (
+    children &&
+    children.length === 1 &&
+    isValidElement(children[0]) &&
+    children[0].type === "svg"
+  )
+}
+
+export { Button, LinkButton, type ButtonProps }
--- app/ui/togglebutton.tsx
+++ app/ui/togglebutton.tsx
@@ -6,32 +6,32 @@ import {
   type ToggleButtonProps as RACToggleButtonProps,
 } from "react-aria-components"
 
-import { compose, cva, cxRenderProps, focusStyle } from "@lib/utils"
+import { compose, cva, cxRenderProps, focusStyle } from "@lib/styleUtils"
 import { Children, isValidElement } from "react"
 
-const styles = compose(
+const style = compose(
   focusStyle,
   cva({
     base: [
       "text-xs font-medium inline-flex items-center justify-center",
       // selected
-      "data-[selected]:text-white",
+      "data-selected:text-white",
       // disabled
-      "data-[disabled]:text-neutral-placeholder data-[disabled]:pointer-events-none data-[disabled]:bg-neutral-bg-subtle",
+      "data-disabled:text-neutral-placeholder data-disabled:bg-neutral-bg-subtle data-disabled:inset-ring data-disabled:inset-ring-neutral-line",
     ],
     variants: {
       variant: { soft: null, ghost: "bg-transparent" },
       intent: {
         neutral:
-          "bg-neutral-bg hover:bg-neutral-bg-hover active:bg-neutral-bg-active text-neutral-text data-[selected]:bg-neutral-solid",
+          "bg-neutral-bg data-hovered:bg-neutral-bg-hover data-pressed:bg-neutral-bg-active text-neutral-text data-selected:bg-neutral-solid",
         accent:
-          "bg-accent-bg hover:bg-accent-bg-hover active:bg-accent-bg-active text-accent-text data-[selected]:bg-accent-solid",
+          "bg-accent-bg data-hovered:bg-accent-bg-hover data-pressed:bg-accent-bg-active text-accent-text data-selected:bg-accent-solid",
         success:
-          "bg-success-bg hover:bg-success-bg-hover active:bg-success-bg-active text-success-text data-[selected]:bg-success-solid",
+          "bg-success-bg data-hovered:bg-success-bg-hover data-pressed:bg-success-bg-active text-success-text data-selected:bg-success-solid",
         warning:
-          "bg-warning-bg hover:bg-warning-bg-hover active:bg-warning-bg-active text-warning-text data-[selected]:bg-warning-solid",
+          "bg-warning-bg data-hovered:bg-warning-bg-hover data-pressed:bg-warning-bg-active text-warning-text data-selected:bg-warning-solid",
         error:
-          "bg-error-bg hover:bg-error-bg-hover active:bg-error-bg-active text-error-text data-[selected]:bg-error-solid",
+          "bg-error-bg data-hovered:bg-error-bg-hover data-pressed:bg-error-bg-active text-error-text data-selected:bg-error-solid",
       },
       size: {
         1: "text-xs h-6 px-1.5 rounded gap-1.5",
@@ -55,37 +55,35 @@ const styles = compose(
 
 interface ToggleButtonProps
   extends RACToggleButtonProps,
-    VariantProps<typeof styles> {}
+    VariantProps<typeof style> {}
 
-function ToggleButton({
-  className,
-  variant,
-  intent,
-  size,
-  isSquare,
-  ...props
-}: ToggleButtonProps) {
-  let children =
-    typeof props.children !== "function" && Children.toArray(props.children)
-  let hasOnlySvg =
-    children &&
-    children.length === 1 &&
-    isValidElement(children[0]) &&
-    children[0].type === "svg"
-
+function ToggleButton({ className, ...props }: ToggleButtonProps) {
   return (
     <RACToggleButton
       {...props}
       className={cxRenderProps(
         className,
-        styles({
-          variant,
-          size,
-          intent,
-          isSquare: typeof isSquare === "boolean" ? isSquare : hasOnlySvg,
+        style({
+          ...props,
+          isSquare:
+            typeof props.isSquare === "boolean" ?
+              props.isSquare
+            : hasOnlySvgChild(props),
         }),
       )}
     />
+  )
+}
+
+function hasOnlySvgChild(props: any): boolean {
+  let children =
+    typeof props.children !== "function" && Children.toArray(props.children)
+
+  return (
+    children &&
+    children.length === 1 &&
+    isValidElement(children[0]) &&
+    children[0].type === "svg"
   )
 }
 
