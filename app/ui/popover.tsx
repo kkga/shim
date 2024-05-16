18444f7eb27aed4c7d6b39c917f4463837d8ae9a
Gadzhi Kharkharov <me@kkga.me>
2024-05-16 12:58:48 +0300

    fix: use theme size in popover

--- app/ui/popover.tsx
+++ app/ui/popover.tsx
@@ -7,31 +7,46 @@ import {
   type PopoverProps as RACPopoverProps,
 } from "react-aria-components"
 
-import { animateMountStyle, compose, cva, cxRenderProps } from "@lib/utils"
+import { animateMountStyle, compose, cva, cxRenderProps } from "@lib/style"
+import { useThemeProps } from "@lib/theme"
 
-const popoverStyle = compose(
+const style = compose(
   animateMountStyle,
   cva({
-    base: "z-20 rounded-lg bg-[var(--color-bg-panel)] text-neutral-text text-sm ring shadow-xl ring-neutral-solid/20 outline-none",
+    base: "z-20 min-w-[var(--trigger-width)] bg-[var(--color-bg-panel)] text-neutral-text text-xs ring shadow-xl ring-neutral-solid/20 outline-none",
+    variants: {
+      size: { 1: "rounded-lg", 2: "rounded-lg", 3: "rounded-[10px]" },
+    },
+    defaultVariants: { size: 1 },
   }),
 )
 
 interface PopoverProps extends Omit<RACPopoverProps, "children"> {
   children: React.ReactNode
+  withDialog?: boolean
 }
 
-function Popover({ className, children, offset = 4, ...props }: PopoverProps) {
+function Popover({
+  className,
+  children,
+  offset = 4,
+  withDialog,
+  ...props
+}: PopoverProps) {
+  let { size } = useThemeProps({})
   return (
     <RACPopover
-      offset={offset}
-      className={cxRenderProps(className, popoverStyle())}
       {...props}
+      offset={offset}
+      className={cxRenderProps(className, style({ size }))}
     >
-      <RACDialog className="outline-none">{children}</RACDialog>
+      {withDialog ?
+        <RACDialog className="overflow-auto outline-none">{children}</RACDialog>
+      : children}
     </RACPopover>
   )
 }
 
 const PopoverTrigger = RACDialogTrigger
 
-export { Popover, PopoverTrigger, popoverStyle, type PopoverProps }
+export { Popover, PopoverTrigger, type PopoverProps }
