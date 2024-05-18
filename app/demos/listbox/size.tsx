38c1eefa334cf583d181064429b65f8bf0bce7a9
Gadzhi Kharkharov <me@kkga.me>
2024-05-18 23:39:42 +0300

    docs: add listbox size demo

---
+++ app/demos/listbox/size.tsx
@@ -0,0 +1,21 @@
+import { ListBox, ListBoxItem } from "@ui/listbox"
+
+export default () => (
+  <div className="flex gap-4">
+    <ListBox size={1} className="min-w-32" selectionMode="multiple" aria-label="Favorite animal">
+      <ListBoxItem>Cat</ListBoxItem>
+      <ListBoxItem>Dog</ListBoxItem>
+      <ListBoxItem>Kangaroo</ListBoxItem>
+    </ListBox>
+    <ListBox size={2} className="min-w-32" selectionMode="multiple" aria-label="Favorite animal">
+      <ListBoxItem>Cat</ListBoxItem>
+      <ListBoxItem>Dog</ListBoxItem>
+      <ListBoxItem>Kangaroo</ListBoxItem>
+    </ListBox>
+    <ListBox size={3} className="min-w-32" selectionMode="multiple" aria-label="Favorite animal">
+      <ListBoxItem>Cat</ListBoxItem>
+      <ListBoxItem>Dog</ListBoxItem>
+      <ListBoxItem>Kangaroo</ListBoxItem>
+    </ListBox>
+  </div>
+)
