11c2ef4d115b44d78c024eb0344469677bd33ff2
Gadzhi Kharkharov <me@kkga.me>
2024-05-19 22:38:45 +0300

    feat: add kbd component

---
+++ app/ui/kbd.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { cva } from "@lib/style"
+import type { VariantProps } from "cva"
+
+const style = cva({
+  base: "inline-flex w-fit items-center bg-panel font-book text-neutral-10 leading-none bg-clip-padding font-sans",
+  variants: {
+    variant: {
+      plain: "bg-transparent",
+      surface: "bg-neutral-bg-subtle shadow-[var(--shadow-xs)]",
+    },
+    size: {
+      1: "h-4 px-[3px] gap-1 rounded-sm text-[11px]",
+      2: "h-5 px-1 gap-1 rounded text-xs",
+      3: "h-6 px-1.5 gap-1.5 rounded-md text-[13px]",
+    },
+  },
+  defaultVariants: {
+    size: 1,
+    variant: "surface",
+  },
+})
+
+interface KbdProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof style> {}
+
+function Kbd({ className, size, variant, ...props }: KbdProps) {
+  return <kbd className={style({ size, variant, className })} {...props} />
+}
+
+export { Kbd, type KbdProps }
