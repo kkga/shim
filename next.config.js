536ec18717066a8d4f7f99ac8ebc4d221474771b
Gadzhi Kharkharov <me@kkga.me>
2024-08-31 13:26:35 +0300

    chore: use react compiler

--- next.config.js
+++ next.config.js
@@ -2,13 +2,16 @@ const localesPlugin = require("@react-aria/optimize-locales-plugin")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
+  experimental: {
+    reactCompiler: true,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.plugins.push(localesPlugin.webpack({ locales: [] }))
     }
     return config
   },
+  transpilePackages: ["next-mdx-remote"],
 }
 
 module.exports = nextConfig
