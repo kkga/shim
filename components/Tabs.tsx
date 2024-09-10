4a550dcd25cc70940456b8dbddd85018aa3c85fa
Gadzhi Kharkharov <me@kkga.me>
2024-09-10 16:21:55 +0400

    feat: improve tabs component, add orientation styling

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import { cva, cxRenderProps } from "@lib/style"
+import { cva, cxRenderProps, VariantProps } from "@lib/style"
+import { Theme, useThemeProps } from "@lib/theme"
+import { createContext, useContext } from "react"
 
 import {
   Tab as RACTab,
@@ -19,7 +21,7 @@ const styles = {
     variants: {
       orientation: {
         horizontal: "flex-col",
-        vertical: "flex-row w-[800px]",
+        vertical: "flex-row",
       },
     },
     defaultVariants: {
@@ -29,57 +31,126 @@ const styles = {
 
   tabList: cva({
     base: [
-      "inline-flex gap-6 w-full items-center justify-start shadow-[inset_0_-1px_0_0_var(--color-neutral-line)]",
+      "group inline-flex items-stretch justify-start",
       // horizontal
-      "data-[orientation=horizontal]:flex-row",
+      "data-[orientation=horizontal]:flex-row data-[orientation=horizontal]:gap-1",
       // vertical
-      "data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start",
+      "data-[orientation=vertical]:flex-col data-[orientation=vertical]:gap-0 data-[orientation=vertical]:px-1",
     ],
+    variants: {
+      variant: {
+        underline: [
+          "data-[orientation=horizontal]:shadow-[inset_0_-1px_0_0_var(--color-neutral-line)]",
+          "data-[orientation=vertical]:shadow-[inset_-1px_0_0_0_var(--color-neutral-line)]",
+        ],
+        soft: "",
+      },
+      size: {
+        1: "text-xs",
+        2: "text-[13px]",
+        3: "text-sm",
+      },
+    },
+    defaultVariants: {
+      variant: "underline",
+      size: 1,
+    },
   }),
 
   tab: cva({
     base: [
-      "border-transparent border-t-2 border-b-2 group inline-flex gap-1.5 h-10 items-center focus-visible:outline-none text-xs font-medium text-neutral-text cursor-default",
+      "relative inline-flex gap-1.5 items-center focus-visible:outline-none font-medium text-neutral-text cursor-default",
+      // indicator
+      "before:absolute after:absolute after:inset-x-0 after:inset-y-1",
+      // horizontal
+      "group-data-[orientation=horizontal]:before:h-0.5 group-data-[orientation=horizontal]:before:inset-x-0 group-data-[orientation=horizontal]:before:bottom-0",
+      // vertical
+      "group-data-[orientation=vertical]:before:w-0.5 group-data-[orientation=vertical]:before:inset-y-1 group-data-[orientation=vertical]:before:-right-1",
       // hover
-      "data-[hovered]:text-neutral-text-contrast",
+      "data-hovered:text-neutral-text-contrast data-hovered:after:bg-neutral-bg-hover data-active:after:bg-neutral-bg-active",
       // selected
-      "data-[selected]:text-neutral-text-contrast data-[selected]:border-b-neutral-text-contrast",
+      "data-selected:text-neutral-text-contrast data-selected:border-b-neutral-text-contrast",
       // disabled
-      "data-[disabled]:text-neutral-placeholder",
+      "data-disabled:text-neutral-placeholder data-disabled:cursor-not-allowed",
     ],
+    variants: {
+      size: {
+        1: "h-8 px-1.5 text-xs after:rounded",
+        2: "h-9 px-2 text-[13px] after:rounded",
+        3: "h-10 px-2.5 text-sm after:rounded-md",
+      },
+      variant: {
+        soft: "data-selected:after:bg-neutral-bg-active",
+        underline: "data-selected:before:bg-neutral-text-contrast",
+      },
+    },
   }),
 
   tabPanel: cva({
-    base: "flex-1 py-2",
+    base: "flex-1",
   }),
 }
 
-function Tabs(props: RACTabsProps) {
+type TabListVariant = "soft" | "underline"
+const TabListVariantContext = createContext<TabListVariant>("underline")
+
+interface TabsProps extends RACTabsProps, VariantProps<typeof styles.tabs> {}
+
+function Tabs(props: TabsProps) {
   return (
     <RACTabs
       {...props}
       className={cxRenderProps(
         props.className,
-        styles.tabs({ orientation: props.orientation }),
+        styles.tabs({
+          orientation: props.orientation,
+        }),
       )}
     />
   )
 }
 
-function TabList<T extends object>(props: RACTabListProps<T>) {
+interface TabListProps<T extends object>
+  extends RACTabListProps<T>,
+    VariantProps<typeof styles.tabList> {}
+
+function TabList<T extends object>(props: TabListProps<T>) {
+  let themeProps = useThemeProps({
+    size: props.size,
+  })
+
   return (
-    <RACTabList
-      {...props}
-      className={cxRenderProps(props.className, styles.tabList())}
-    />
+    <Theme {...themeProps}>
+      <TabListVariantContext.Provider value={props.variant || "underline"}>
+        <RACTabList
+          {...props}
+          className={cxRenderProps(
+            props.className,
+            styles.tabList({
+              variant: props.variant,
+              size: themeProps.size,
+            }),
+          )}
+        />
+      </TabListVariantContext.Provider>
+    </Theme>
   )
 }
 
 function Tab(props: RACTabProps) {
+  let variant = useContext(TabListVariantContext)
+  let { size } = useThemeProps()
+
   return (
     <RACTab
       {...props}
-      className={cxRenderProps(props.className, styles.tab())}
+      className={cxRenderProps(
+        props.className,
+        styles.tab({
+          variant,
+          size,
+        }),
+      )}
     />
   )
 }
@@ -94,3 +165,4 @@ function TabPanel(props: RACTabPanelProps) {
 }
 
 export { Tab, TabList, TabPanel, Tabs }
+export type { TabsProps, TabListProps }
