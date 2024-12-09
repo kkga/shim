ab0b3b79c04c86243968c3cb82591ee9df62da41
Gadzhi Kharkharov <me@kkga.me>
2024-12-09 09:11:52 +0200

    feat: add Avatar component

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { VariantProps, cva, cx } from "@lib/style"
 import { useEffect, useState } from "react"
+import { VariantProps, tv } from "tailwind-variants"
 
 const colors = {
   gray: "text-[var(--slate-2)] bg-[var(--slate-9)]",
@@ -12,28 +12,39 @@ const colors = {
 
 type Color = keyof typeof colors
 
-const style = cva({
-  base: [
-    "relative rounded-full overflow-hidden font-medium flex items-center justify-center text-neutral-text cursor-default",
-    "before:absolute before:inset-0 before:rounded-full before:inset-ring before:inset-ring-neutral-line before:pointer-events-none",
-  ],
+const style = tv({
+  slots: {
+    base: [
+      "shrink-0 relative tracking-tight rounded-full overflow-hidden font-medium flex items-center justify-center text-neutral-text cursor-default",
+      "before:absolute before:inset-0 before:rounded-full before:inset-ring before:inset-ring-neutral-line before:pointer-events-none",
+    ],
+    img: "size-full select-none object-cover",
+  },
   variants: {
     size: {
-      1: "size-5 text-[11px]",
-      2: "size-6 text-xs",
-      3: "size-8 text-[13px]",
+      1: "size-5 text-[10px]",
+      2: "size-6 text-[11px]",
+      3: "size-8 text-[12px]",
+      4: "size-10 text-sm",
+      5: "size-12 text-base",
+      6: "size-16 text-lg",
     },
     color: colors,
+    loading: {
+      true: {
+        img: "hidden",
+      },
+    },
   },
   defaultVariants: {
-    color: "gray",
+    color: "blue",
     size: 2,
   },
 })
 
 interface AvatarProps
   extends React.ImgHTMLAttributes<HTMLImageElement>,
-    VariantProps<typeof style> {
+    Omit<VariantProps<typeof style>, "loading"> {
   name: string
   src?: string
   color?: Color
@@ -47,9 +58,12 @@ enum Status {
 }
 
 function Avatar({ src, name, size, color, className }: AvatarProps) {
-  let [status, setStatus] = useState<Status>(src ? Status.Loading : Status.Idle)
+  const [status, setStatus] = useState<Status>(
+    src ? Status.Loading : Status.Idle,
+  )
+  const { base, img } = style({ size, color })
 
-  let initials = name
+  const initials = name
     ?.split(" ")
     .map((chunk) => chunk.charAt(0).toLocaleUpperCase())
     .slice(0, 2)
@@ -72,21 +86,14 @@ function Avatar({ src, name, size, color, className }: AvatarProps) {
     }
   }, [src])
 
-  let isLoading = status === Status.Loading
-  let hasLoadedImage = status === Status.Success
+  const isLoading = status === Status.Loading
+  const hasLoadedImage = status === Status.Success
 
   return (
-    <div className={style({ size, color, className })}>
+    <div className={base({ size, color, className })}>
       {src && (isLoading || hasLoadedImage) && (
         // eslint-disable-next-line @next/next/no-img-element
-        <img
-          src={src}
-          alt={name}
-          className={cx(
-            "size-full select-none object-cover",
-            isLoading ? "hidden" : "block",
-          )}
-        />
+        <img src={src} alt={name} className={img({ loading: isLoading })} />
       )}
 
       {!hasLoadedImage && !isLoading && <span>{initials}</span>}
@@ -94,4 +101,5 @@ function Avatar({ src, name, size, color, className }: AvatarProps) {
   )
 }
 
-export { Avatar, type AvatarProps }
+export { Avatar }
+export type { AvatarProps }
---
+++ docs/Avatar/size.tsx
@@ -0,0 +1,12 @@
+import { Avatar } from "@ui/Avatar"
+
+export default () => (
+  <div className="flex gap-2">
+    <Avatar size={1} name="Kale Frost" />
+    <Avatar size={2} name="Kale Frost" />
+    <Avatar size={3} name="Kale Frost" />
+    <Avatar size={4} name="Kale Frost" />
+    <Avatar size={5} name="Kale Frost" />
+    <Avatar size={6} name="Kale Frost" />
+  </div>
+)
