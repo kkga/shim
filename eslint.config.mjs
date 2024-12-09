6e8ce47489e1e76a4ddf290b09e68ba11b58e967
Gadzhi Kharkharov <me@kkga.me>
2024-12-09 09:12:04 +0200

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,10 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ["docs/**/*"],
     rules: {
       "prefer-const": "off",
     },
+  },
+  {
+    ignores: ["docs/"],
   },
 ]
 
