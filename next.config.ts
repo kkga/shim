7096e4ee4696cc1b9175532534f9111fc211e353
Gadzhi Kharkharov <me@kkga.me>
2024-12-11 09:27:32 +0200

    chore: update packages

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-import localesPlugin from "@react-aria/optimize-locales-plugin"
 import { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.plugins.push(localesPlugin.webpack({ locales: ["en-US"] }))
-    }
-    return config
   },
   transpilePackages: ["next-mdx-remote"],
 }
