d04e46e872bcad9a95eb07f3757ca6b621dbea7d
Gadzhi Kharkharov <me@kkga.me>
2025-12-03 18:57:08 +0200

    fix: ToggleButton: improve hover/active styles in selected state

--- shim-ui/toggle-button.tsx
+++ shim-ui/toggle-button.tsx
@@ -9,12 +9,16 @@ import {
 } from "react-aria-components";
 import { type ClassValue, tv, type VariantProps } from "tailwind-variants";
 import { INTENTS, type Intent } from "@/shim-ui/lib/style";
-import { useThemeProps } from "@/shim-ui/lib/theme";
+import { buildVariantOverrides, useThemeProps } from "@/shim-ui/lib/theme";
 
 const style = tv({
   base: "focus-ring inline-flex shrink-0 items-center justify-center font-book font-sans leading-none!",
   variants: {
-    variant: { soft: "", ghost: "bg-transparent" },
+    variant: {
+      soft: "",
+      solid: "",
+      ghost: "bg-transparent",
+    },
     intent: INTENTS.reduce(
       (acc, intent) => {
         acc[intent] = "";
@@ -42,7 +46,7 @@ const style = tv({
   compoundVariants: [
     {
       isInGroup: true,
-      variant: "soft",
+      variant: ["soft", "solid"],
       class:
         "not-first:not-last:rounded-none first:rounded-r-none last:rounded-l-none",
     },
@@ -56,47 +60,57 @@ const style = tv({
     { size: 4, isIconOnly: true, isInGroup: false, class: "size-10 p-0" },
     {
       intent: "neutral",
-      variant: ["soft", "ghost"],
-      class:
-        "text-neutral-text data-hovered:bg-neutral-bg-hover data-pressed:bg-neutral-bg-active data-selected:bg-neutral-solid data-selected:text-white",
+      variant: ["soft", "solid", "ghost"],
+      class: [
+        "text-neutral-text data-hovered:bg-neutral-bg-hover data-pressed:bg-neutral-bg-active",
+        "data-selected:data-hovered:bg-neutral-solid-hover data-selected:data-pressed:brightness-90 data-selected:bg-neutral-solid data-selected:text-white",
+      ],
     },
-    { intent: "neutral", variant: "soft", class: "bg-neutral-bg" },
+    { intent: "neutral", variant: ["soft", "solid"], class: "bg-neutral-bg" },
     { intent: "neutral", variant: "ghost", class: "bg-transparent" },
     {
       intent: "accent",
-      variant: ["soft", "ghost"],
-      class:
-        "text-accent-text data-hovered:bg-accent-bg-hover data-pressed:bg-accent-bg-active data-selected:bg-accent-solid data-selected:text-white",
+      variant: ["soft", "solid", "ghost"],
+      class: [
+        "text-accent-text data-hovered:bg-accent-bg-hover data-pressed:bg-accent-bg-active",
+        "data-selected:data-hovered:bg-accent-solid-hover data-selected:data-pressed:brightness-90 data-selected:bg-accent-solid data-selected:text-white",
+      ],
     },
-    { intent: "accent", variant: "soft", class: "bg-accent-bg" },
+    { intent: "accent", variant: ["soft", "solid"], class: "bg-accent-bg" },
     { intent: "accent", variant: "ghost", class: "bg-transparent" },
     {
       intent: "success",
-      variant: ["soft", "ghost"],
-      class:
-        "text-success-text data-hovered:bg-success-bg-hover data-pressed:bg-success-bg-active data-selected:bg-success-solid data-selected:text-white",
+      variant: ["soft", "solid", "ghost"],
+      class: [
+        "text-success-text data-hovered:bg-success-bg-hover data-pressed:bg-success-bg-active",
+        "data-selected:data-hovered:bg-success-solid-hover data-selected:data-pressed:brightness-90 data-selected:bg-success-solid data-selected:text-white",
+      ],
     },
-    { intent: "success", variant: "soft", class: "bg-success-bg" },
+    { intent: "success", variant: ["soft", "solid"], class: "bg-success-bg" },
     { intent: "success", variant: "ghost", class: "bg-transparent" },
     {
       intent: "warning",
-      variant: ["soft", "ghost"],
-      class:
-        "text-warning-text data-hovered:bg-warning-bg-hover data-pressed:bg-warning-bg-active data-selected:bg-warning-solid data-selected:text-white",
+      variant: ["soft", "solid", "ghost"],
+      class: [
+        "text-warning-text data-hovered:bg-warning-bg-hover data-pressed:bg-warning-bg-active",
+        "data-selected:data-hovered:bg-warning-solid-hover data-selected:data-pressed:brightness-90 data-selected:bg-warning-solid data-selected:text-white",
+      ],
     },
-    { intent: "warning", variant: "soft", class: "bg-warning-bg" },
+    { intent: "warning", variant: ["soft", "solid"], class: "bg-warning-bg" },
     { intent: "warning", variant: "ghost", class: "bg-transparent" },
     {
       intent: "danger",
-      variant: ["soft", "ghost"],
-      class:
-        "text-danger-text data-hovered:bg-danger-bg-hover data-pressed:bg-danger-bg-active data-selected:bg-danger-solid data-selected:text-white",
+      variant: ["soft", "solid", "ghost"],
+      class: [
+        "text-danger-text data-hovered:bg-danger-bg-hover data-pressed:bg-danger-bg-active",
+        "data-selected:data-hovered:bg-danger-solid-hover data-selected:data-pressed:brightness-90 data-selected:bg-danger-solid data-selected:text-white",
+      ],
     },
-    { intent: "danger", variant: "soft", class: "bg-danger-bg" },
+    { intent: "danger", variant: ["soft", "solid"], class: "bg-danger-bg" },
     { intent: "danger", variant: "ghost", class: "bg-transparent" },
     {
       isDisabled: true,
-      variant: ["soft"],
+      variant: ["soft", "solid"],
       intent: ["neutral", "accent", "success", "warning", "danger"],
       class: "bg-neutral-bg! text-neutral-text-subtle",
     },
@@ -116,21 +130,13 @@ const style = tv({
 
 interface ToggleButtonProps
   extends RacToggleButtonProps,
-    Omit<VariantProps<typeof style>, "isInGroup"> {
-  variant?: "soft" | "ghost";
-}
+    Omit<VariantProps<typeof style>, "isInGroup"> {}
 
-function ToggleButton({
-  variant: _variant,
-  size: _size,
-  intent,
-  isIconOnly,
-  ...props
-}: ToggleButtonProps) {
+function ToggleButton({ intent, isIconOnly, ...props }: ToggleButtonProps) {
   const isInGroup = useContext(ToggleGroupStateContext) !== null;
   const { variants, size } = useThemeProps({
-    size: _size,
-    variants: _variant ? { button: _variant } : undefined,
+    ...props,
+    ...buildVariantOverrides("button", props.variant),
   });
 
   return (
@@ -142,7 +148,7 @@ function ToggleButton({
           isInGroup,
           size,
           intent,
-          variant: variants.button as "soft" | "ghost",
+          variant: variants.button,
           isIconOnly:
             typeof isIconOnly === "boolean"
               ? isIconOnly
