3af65af58269d203cfca81f21d61013c525add86
Gadzhi Kharkharov <me@kkga.me>
2025-12-03 23:45:34 +0200

    feat(separator): support size prop

--- shim-ui/separator.tsx
+++ shim-ui/separator.tsx
@@ -4,7 +4,8 @@ import {
   Separator as RacSeparator,
   type SeparatorProps as RacSeparatorProps,
 } from "react-aria-components";
-import { tv } from "tailwind-variants";
+import { tv, type VariantProps } from "tailwind-variants";
+import { useThemeProps } from "@/shim-ui/lib/theme";
 
 const styles = tv({
   base: "shrink-0 self-stretch border-none bg-neutral-line",
@@ -13,25 +14,73 @@ const styles = tv({
       horizontal: "h-px w-full",
       vertical: "w-px",
     },
+    size: { 1: "", 2: "", 3: "", 4: "" },
   },
+  compoundVariants: [
+    {
+      orientation: "vertical",
+      size: 1,
+      className: "mx-2",
+    },
+    {
+      orientation: "vertical",
+      size: 2,
+      className: "mx-3",
+    },
+    {
+      orientation: "vertical",
+      size: 3,
+      className: "mx-4",
+    },
+    {
+      orientation: "vertical",
+      size: 4,
+      className: "mx-6",
+    },
+    {
+      orientation: "horizontal",
+      size: 1,
+      className: "my-2",
+    },
+    {
+      orientation: "horizontal",
+      size: 2,
+      className: "my-3",
+    },
+    {
+      orientation: "horizontal",
+      size: 3,
+      className: "my-4",
+    },
+    {
+      orientation: "horizontal",
+      size: 4,
+      className: "my-6",
+    },
+  ],
   defaultVariants: {
     orientation: "horizontal",
   },
 });
 
-function Separator(props: RacSeparatorProps) {
+interface SeparatorProps
+  extends RacSeparatorProps,
+    VariantProps<typeof styles> {}
+
+function Separator(props: SeparatorProps) {
+  const { size } = useThemeProps({ size: props.size });
+
   return (
     <RacSeparator
       {...props}
       className={styles({
         orientation: props.orientation,
+        size,
         className: props.className,
       })}
     />
   );
 }
-
-interface SeparatorProps extends RacSeparatorProps {}
 
 export { Separator };
 export type { SeparatorProps };
