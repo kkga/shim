3933487b1393b48d2cfc0c01ccb1387718bd1907
Gadzhi Kharkharov <me@kkga.me>
2025-12-08 10:59:53 +0200

    refactor: remove separate color-thumb component, move into color-area and color-slider

--- shim-ui/color-area.tsx
+++ shim-ui/color-area.tsx
@@ -3,30 +3,64 @@
 import {
   ColorArea as RacColorArea,
   type ColorAreaProps as RacColorAreaProps,
+  ColorThumb as RacColorThumb,
+  type ColorThumbProps as RacColorThumbProps,
 } from "react-aria-components";
-import { tv, type VariantProps } from "tailwind-variants";
-import { ColorThumb } from "@/shim-ui/color-thumb";
+import { type ClassValue, tv, type VariantProps } from "tailwind-variants";
 import { cnRenderProps } from "@/shim-ui/lib/style";
+import type { Size } from "@/shim-ui/lib/theme";
 
-const style = tv({
+const thumbStyle = tv({
+  base: "focus-ring top-[50%] left-[50%] rounded-full border-3 border-white",
+  variants: {
+    size: {
+      1: "size-5",
+      2: "size-6",
+      3: "size-7",
+      4: "size-9",
+    } satisfies Record<Size, ClassValue>,
+  },
+});
+
+interface ColorThumbProps
+  extends RacColorThumbProps,
+    VariantProps<typeof thumbStyle> {}
+
+function ColorThumb({ size, ...props }: ColorThumbProps) {
+  return (
+    <RacColorThumb
+      {...props}
+      className={() => thumbStyle({ size })}
+      style={({ defaultStyle }) => ({
+        ...defaultStyle,
+        backgroundImage:
+          "repeating-conic-gradient(#CCC 0% 25%, white 0% 50%) 50% / 16px 16px",
+        boxShadow:
+          "0 0 0 1px var(--black-a3), var(--shadow-inner), var(--shadow-xs)",
+      })}
+    />
+  );
+}
+
+const areaStyle = tv({
   base: "size-56 rounded-sm bg-neutral-panel shadow-inner",
 });
 
 interface ColorAreaProps
   extends RacColorAreaProps,
-    VariantProps<typeof style> {}
+    VariantProps<typeof areaStyle> {}
 
 function ColorArea(props: ColorAreaProps) {
   return (
     <RacColorArea
       {...props}
-      className={cnRenderProps(props.className, style())}
+      className={cnRenderProps(props.className, areaStyle())}
       style={({ defaultStyle, isDisabled }) => ({
         ...defaultStyle,
         background: isDisabled ? undefined : defaultStyle.background,
       })}
     >
-      <ColorThumb variant="loupe" />
+      <ColorThumb size={1} />
     </RacColorArea>
   );
 }
--- shim-ui/color-slider.tsx
+++ shim-ui/color-slider.tsx
@@ -3,15 +3,98 @@
 import {
   ColorSlider as RacColorSlider,
   type ColorSliderProps as RacColorSliderProps,
+  ColorThumb as RacColorThumb,
+  type ColorThumbProps as RacColorThumbProps,
   SliderOutput as RacSliderOutput,
   SliderTrack as RacSliderTrack,
 } from "react-aria-components";
-import type { VariantProps } from "tailwind-variants";
-import { ColorThumb } from "@/shim-ui/color-thumb";
+import { type ClassValue, tv, type VariantProps } from "tailwind-variants";
 import { fieldLayoutStyle, Label } from "@/shim-ui/field";
 import { cnRenderProps } from "@/shim-ui/lib/style";
-import { Theme, useThemeProps } from "@/shim-ui/lib/theme";
+import { type Size, Theme, useThemeProps } from "@/shim-ui/lib/theme";
 import { style } from "@/shim-ui/slider";
+
+const thumbStyle = tv({
+  base: "focus-ring top-[50%] left-[50%] rounded-sm border-3 border-white",
+  variants: {
+    size: {
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+    } satisfies Record<Size, ClassValue>,
+    orientation: {
+      horizontal: "",
+      vertical: "",
+    },
+  },
+  compoundVariants: [
+    {
+      orientation: "horizontal",
+      size: 1,
+      className: "h-7.5 w-3",
+    },
+    {
+      orientation: "horizontal",
+      size: 2,
+      className: "h-8.5 w-3",
+    },
+    {
+      orientation: "horizontal",
+      size: 3,
+      className: "h-9.5 w-3.5",
+    },
+    {
+      orientation: "horizontal",
+      size: 4,
+      className: "h-12 w-4.5 rounded-[5px] border-4",
+    },
+    {
+      orientation: "vertical",
+      size: 1,
+      className: "h-3 w-7.5",
+    },
+    {
+      orientation: "vertical",
+      size: 2,
+      className: "h-3 w-8.5",
+    },
+    {
+      orientation: "vertical",
+      size: 3,
+      className: "h-3.5 w-9.5",
+    },
+    {
+      orientation: "vertical",
+      size: 4,
+      className: "h-4.5 w-12 rounded-[5px] border-4",
+    },
+  ],
+});
+
+interface ColorThumbProps
+  extends RacColorThumbProps,
+    VariantProps<typeof thumbStyle> {}
+
+function ColorThumb({
+  size = 1,
+  orientation = "horizontal",
+  ...props
+}: ColorThumbProps) {
+  return (
+    <RacColorThumb
+      {...props}
+      className={() => thumbStyle({ size, orientation })}
+      style={({ defaultStyle }) => ({
+        ...defaultStyle,
+        backgroundImage:
+          "repeating-conic-gradient(#CCC 0% 25%, white 0% 50%) 50% / 16px 16px",
+        boxShadow:
+          "0 0 0 1px var(--black-a3), var(--shadow-inner), var(--shadow-xs)",
+      })}
+    />
+  );
+}
 
 interface ColorSliderProps
   extends RacColorSliderProps,
